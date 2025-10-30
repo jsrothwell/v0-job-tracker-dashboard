a7712f609ba956ef01b4d92ca8be6b0b754060fd
Jamie Rothwell <jamiesonrothwell@gmail.com>
2025-10-30 13:42:51 -0700

    Reverted bad change and added max-h-full to job detail area for scrolling

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -17,9 +17,9 @@ function ScrollArea({
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
-        data-slot="scroll-area-viewport"
-        className="focus-visible:ring-ring/50 w-full h-0 rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
-      >
+        data-slot="scroll-area-viewport"
+        className="focus-visible:ring-ring/50 size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:outline-1"
+      >
         {children}
       </ScrollAreaPrimitive.Viewport>
       <ScrollBar />
